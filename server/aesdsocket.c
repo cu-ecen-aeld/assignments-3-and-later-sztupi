0470d275c1555d5e92b47f532fd126714d420123
Attila Sztupak <mail@attila.sztupak.name>
2024-05-31 10:57:16 +0100

    assignment 5 refactored

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -21,6 +21,192 @@ void handle_signal(int sig) {
   }
 }
 
+int listen_socket(int daemon, struct addrinfo **servinfo) {
+  int socketfd = socket(PF_INET, SOCK_STREAM, 0);
+  if (socketfd == -1) {
+    syslog(LOG_ERR, "Error allocating socket: %s", strerror(errno));
+    return -1;
+  }
+
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  int status = getaddrinfo(NULL, "9000", &hints, servinfo);
+  if (status != 0) {
+    syslog(LOG_ERR, "Error getting addrinfo: %s", strerror(errno));
+    return -1;
+  }
+
+  status = bind(socketfd, (*servinfo)->ai_addr, (*servinfo)->ai_addrlen);
+  if (status != 0) {
+    syslog(LOG_ERR, "Error binding socket to port: %s", strerror(errno));
+    return -1;
+  }
+  syslog(LOG_DEBUG, "Bound to port");
+
+  if (daemon) {
+    int pid = fork();
+    if (pid < 0) {
+      syslog(LOG_ERR, "Error forking daemon process: %s", strerror(errno));
+      exit(-1);
+    } else if (pid > 0) {
+      exit(0);
+    }
+  }
+
+  status = listen(socketfd, 5);
+  if (status != 0) {
+    syslog(LOG_ERR, "Error listening: %s", strerror(errno));
+    return -1;
+  }
+  syslog(LOG_DEBUG, "Listening on port");
+
+  return socketfd;
+}
+
+int wait_for_connection(int socketfd, struct sockaddr_storage * addr) {
+  fd_set read_fds;
+  FD_ZERO(&read_fds);
+  FD_SET(socketfd, &read_fds);
+  syslog(LOG_DEBUG, "Waiting for connection");
+  int selectres = select(socketfd+1, &read_fds, NULL, NULL, NULL);
+  if (selectres == -1) {
+    if (errno == EINTR) {
+      return 0;
+    } else {
+      syslog(LOG_ERR, "Error waiting for connection: %s", strerror(errno));
+      return -1;
+    }
+  }
+  syslog(LOG_DEBUG, "Incoming connection");
+
+  socklen_t addr_size;
+  addr_size = sizeof(&addr);
+
+  int connfd = accept(socketfd, (struct sockaddr *)addr, &addr_size);
+  if (connfd == -1) {
+    syslog(LOG_ERR, "Error accepting connection: %s", strerror(errno));
+    return -1;
+  }
+  syslog(LOG_INFO, "Accepted connection from %s",
+          inet_ntoa(((struct sockaddr_in *)addr)->sin_addr));
+
+  return connfd;
+}
+
+int wait_for_data(int connfd) {
+  fd_set read_fds;
+  FD_ZERO(&read_fds);
+  FD_SET(connfd, &read_fds);
+  int selectres = select(connfd + 1, &read_fds, NULL, NULL, NULL);
+  if (selectres == -1) {
+    if (errno == EINTR) {
+      return 0;
+    } else {
+      syslog(LOG_ERR, "Error waiting for recieve: %s", strerror(errno));
+      return -1;
+    }
+  }
+  return 1;
+}
+
+const int BUF_SIZE = 256;
+
+struct stream_data {
+  ssize_t size;
+  ssize_t pos;
+  ssize_t unprocessed;
+  char *buf;
+};
+
+void stream_allocate(struct stream_data *stream) {
+  stream->size = BUF_SIZE;
+  stream->pos = 0;
+  stream->buf = (char *)calloc(stream->size, sizeof(char));
+  stream->unprocessed = 0;
+}
+
+void stream_free(struct stream_data *stream) {
+  free(stream->buf);
+}
+
+void stream_extend_buffer(struct stream_data *stream) {
+  stream->size = stream->size * 2;
+  char *new_buf = (char *)calloc(stream->size, sizeof(char));
+  strncpy(new_buf, stream->buf, stream->pos + stream->unprocessed);
+  free(stream->buf);
+  stream->buf = new_buf;
+}
+
+int stream_has_space(struct stream_data *stream, int len) {
+  return stream->pos + stream->unprocessed + len < stream->size;
+}
+
+void stream_append_data(struct stream_data *stream, char* buf, int len) {
+  while (!stream_has_space(stream, len)) stream_extend_buffer(stream);
+  strncpy(stream->buf + stream->pos + stream->unprocessed, buf, len);
+  stream->unprocessed += len;
+}
+
+int stream_receive(int connfd, struct stream_data *stream) {
+  char recv_buf[BUF_SIZE];
+  ssize_t recv_len = recv(connfd, (void *)recv_buf, BUF_SIZE, 0); // MSG_WAITALL?
+  if (recv_len < 0) {
+    syslog(LOG_ERR, "Error receiving: %s", strerror(errno));
+  }
+  if (recv_len == 0) {
+    return 0;
+  } else {
+    stream_append_data(stream, recv_buf, recv_len);
+  }
+  return 1;
+}
+
+int stream_process(struct stream_data *stream) {
+  while (stream->unprocessed > 0 && stream->buf[stream->pos] != '\n') {
+    stream->unprocessed -= 1;
+    stream->pos += 1;
+  }
+  return stream->buf[stream->pos] == '\n';
+}
+
+void stream_write(struct stream_data *stream, int fd) {
+  write(fd, stream->buf, stream->pos+1);
+  strncpy(stream->buf, stream->buf + stream->pos+1, stream->unprocessed);
+  stream->pos = 0;
+}
+
+void send_data(int fdTarget, int fdSource) {
+  char send_buf[256];
+  ssize_t send_len = read(fdSource, send_buf, 256);
+  if (send_len < 0) {
+    syslog(LOG_ERR, "Error while reading from temp file: %s", strerror(errno));
+    exit(-1);
+  }
+  while (send_len > 0) {
+    char msg[257];
+    strncpy(msg, send_buf, send_len);
+    msg[send_len] = '\0';
+    syslog(LOG_DEBUG, "Sending %s", msg);
+    ssize_t total_sent = 0;
+    while (total_sent < send_len) {
+      ssize_t sent =
+          send(fdTarget, send_buf + total_sent, send_len - total_sent, 0);
+      syslog(LOG_DEBUG, "Sent %d", (int)sent);
+      total_sent += sent;
+    }
+    send_len = read(fdSource, send_buf, 256);
+    if (send_len < 0) {
+      syslog(LOG_ERR, "Error while reading from temp file: %s",
+             strerror(errno));
+      exit(-1);
+    }
+  }
+}
+
 int main(int argc, char* argv[]) {
   int opt;
   int daemon = 0;
@@ -37,157 +223,45 @@ int main(int argc, char* argv[]) {
   signal(SIGTERM, handle_signal);
   signal(SIGINT, handle_signal);
 
-  int socketfd = socket(PF_INET, SOCK_STREAM, 0);
-  if (socketfd == -1) {
-    syslog(LOG_ERR, "Error allocating socket: %s", strerror(errno));
-    return -1;
-  }
-
-  struct addrinfo hints, *servinfo;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
-
-  int status = getaddrinfo(NULL, "9000", &hints, &servinfo);
-  if (status != 0) {
-    syslog(LOG_ERR, "Error getting addrinfo: %s", strerror(errno));
-    return -1;
-  }
-
-  status = bind(socketfd, servinfo->ai_addr, servinfo->ai_addrlen);
-  if (status != 0) {
-    syslog(LOG_ERR, "Error binding socket to port: %s", strerror(errno));
-    return -1;
-  }
-  syslog(LOG_DEBUG, "Bound to port");
-
-  if (daemon) {
-    int pid = fork();
-    if (pid < 0) {
-      syslog(LOG_ERR, "Error forking daemon process: %s", strerror(errno));
-      return -1;
-    } else if (pid > 0) {
-      return 0;
-    }
-  }
-
-
-  status = listen(socketfd, 5);
-  if (status != 0) {
-    syslog(LOG_ERR, "Error listening: %s", strerror(errno));
-    return -1;
-  }
-  syslog(LOG_DEBUG, "Listening on port");
-
-  struct sockaddr_storage addr;
-  socklen_t addr_size;
-  addr_size = sizeof(addr);
+  struct addrinfo *servinfo;
+  int socketfd = listen_socket(daemon, &servinfo);
+  if (socketfd == -1) exit(-1);
 
   while (!terminated) {
-    fd_set read_fds;
-    FD_ZERO(&read_fds);
-    FD_SET(socketfd, &read_fds);
-    syslog(LOG_DEBUG, "Waiting for connection");
-    int selectres = select(socketfd+1, &read_fds, NULL, NULL, NULL);
-    if (selectres == -1) {
-      if (errno == EINTR) {
-        continue;
-      } else {
-        syslog(LOG_ERR, "Error waiting for connection: %s", strerror(errno));
-        return -1;
-      }
-    }
-    syslog(LOG_DEBUG, "Incoming connection");
+    struct sockaddr_storage conn_addr;
+    int connfd = wait_for_connection(socketfd, &conn_addr);
+    if (connfd == -1)
+      exit(-1);
+    else if (connfd == 0)
+      continue;
 
-    int connfd = accept(socketfd, (struct sockaddr *)&addr, &addr_size);
-    if (connfd == -1) {
-      syslog(LOG_ERR, "Error accepting connection: %s", strerror(errno));
-      return -1;
-    }
-    syslog(LOG_INFO, "Accepted connection from %s",
-           inet_ntoa(((struct sockaddr_in *)&addr)->sin_addr));
+    struct stream_data stream;
+    stream_allocate(&stream);
 
-    ssize_t stream_size = 256;
-    ssize_t stream_pos = 0;
-    char *stream_buf = (char *)calloc(stream_size, sizeof(char));
+    int client_eof = 0;
+    while (!client_eof && !terminated) {
+      int status = wait_for_data(connfd);
+      if (status == -1) exit(-1);
+      else if (status == 0) continue;
 
-    int stop = 0;
-    while (!stop && !terminated) {
-      FD_ZERO(&read_fds);
-      FD_SET(connfd, &read_fds);
-      selectres = select(connfd+1, &read_fds, NULL, NULL, NULL);
-      if (selectres == -1) {
-        if (errno == EINTR) {
-          continue;
-        } else {
-          syslog(LOG_ERR, "Error waiting for recieve: %s", strerror(errno));
-          return -1;
-        }
-      }
+      status = stream_receive(connfd, &stream);
+      if (status == 0) client_eof = 1;
 
-      char recv_buf[256];
-      ssize_t recv_len = recv(connfd, (void *)recv_buf, 256, 0); // MSG_WAITALL?
-      if (recv_len < 0) {
-        syslog(LOG_ERR, "Error receiving: %s", strerror(errno));
-      }
-      if (recv_len == 0) {
-        stop = 1;
-      } else {
-        if (stream_pos + recv_len >= stream_size) {
-          stream_size = stream_size * 2;
-          char *new_buf = (char *)calloc(stream_size, sizeof(char));
-          strncpy(new_buf, stream_buf, stream_pos);
-          free(stream_buf);
-          stream_buf = new_buf;
-        }
-        strncpy(stream_buf+stream_pos, recv_buf, recv_len);
-      }
-      while (recv_len > 0) {
-        stream_pos += 1;
-        recv_len -= 1;
-        if (stream_buf[stream_pos] == '\n') {
-          int fdTarget = open("/var/tmp/aesdsocketdata", O_CREAT | O_APPEND | O_WRONLY, 0644);
-          write(fdTarget, stream_buf, stream_pos+1);
-          close(fdTarget);
+      while (stream_process(&stream)) {
+        int fdtarget = open("/var/tmp/aesdsocketdata", O_CREAT | O_APPEND | O_WRONLY, 0644);
+        stream_write(&stream, fdtarget);
+        close(fdtarget);
 
-          fdTarget = open("/var/tmp/aesdsocketdata", O_RDONLY);
-          char send_buf[256];
-          ssize_t send_len = read(fdTarget, send_buf, 256);
-          if (send_len < 0) {
-            syslog(LOG_ERR, "Error while reading from temp file: %s", strerror(errno));
-            exit(-1);
-          }
-          while (send_len > 0) {
-            char msg[257];
-            strncpy(msg, send_buf, send_len);
-            msg[send_len] = '\0';
-            syslog(LOG_DEBUG, "Sending %s", msg);
-            ssize_t total_sent = 0;
-            while (total_sent < send_len) {
-              ssize_t sent =
-                  send(connfd, send_buf + total_sent, send_len - total_sent, 0);
-              syslog(LOG_DEBUG, "Sent %d", (int)sent);
-              total_sent += sent;
-            }
-            send_len = read(fdTarget, send_buf, 256);
-            if (send_len < 0) {
-              syslog(LOG_ERR, "Error while reading from temp file: %s", strerror(errno));
-              exit(-1);
-            }
-          }
-          close(fdTarget);
-
-          strncpy(stream_buf, stream_buf+stream_pos+1, recv_len);
-          stream_pos = -1;
-        }
+        int fddata = open("/var/tmp/aesdsocketdata", O_RDONLY);
+        send_data(connfd, fddata);
+        close(fddata);
       }
     }
 
-    free(stream_buf);
+    stream_free(&stream);
 
     syslog(LOG_INFO, "Closed connection from %s",
-           inet_ntoa(((struct sockaddr_in *)&addr)->sin_addr));
+           inet_ntoa(((struct sockaddr_in *)&conn_addr)->sin_addr));
   }
 
   syslog(LOG_INFO, "Caught signal, exiting");
